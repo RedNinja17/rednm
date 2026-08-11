e6f7fada9915d6cee5292fd9d93af5cfc65cb80d
RedNinja17 <redninjanm17@gmail.com>
2026-08-11 12:18:23 -0400

    Cleaned up some of index.html

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const dock = document.getElementById("actionDock");
-  const trigger = document.getElementById("dockTrigger");
+  const dock = document.getElementById("dock");
+  const dockBtn = document.getElementById("dock-btn");
   const settingsBtn = document.getElementById("settings-btn");
-  const settingsPopup = document.getElementById("settings-popup");
+  const settingsPopup = document.getElementById("settings-pop-up");
 
-  trigger?.addEventListener("click", (e) => {
+  dockBtn?.addEventListener("click", (e) => {
     e.stopPropagation();
     dock.classList.toggle("expanded");
   });
